0b79f42ffe7229ed4f907a194602d3b97aee4925
B-oer1 <boladele1@byupathway.edu>
2026-06-18 01:17:54 +0100

    images adjusment

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -33,7 +33,7 @@ const temples = [
     {
         templeName: "Aba Nigeria",
         location: "Aba, Nigeria",
-        dedicated: "2005, August, 7",
+        dedication: "2005, August, 7",
         area: 11500,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
@@ -41,7 +41,7 @@ const temples = [
     {
         templeName: "Manti Utah",
         location: "Manti, Utah, United States",
-        dedicated: "1888, May, 21",
+        dedication: "1888, May, 21",
         area: 74792,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
@@ -49,7 +49,7 @@ const temples = [
     {
         templeName: "Payson Utah",
         location: "Payson, Utah, United States",
-        dedicated: "2015, June, 7",
+        dedication: "2015, June, 7",
         area: 96630,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
@@ -57,7 +57,7 @@ const temples = [
     {
         templeName: "Yigo Guam",
         location: "Yigo, Guam",
-        dedicated: "2020, May, 2",
+        dedication: "2020, May, 2",
         area: 6861,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
@@ -65,7 +65,7 @@ const temples = [
     {
         templeName: "Washington D.C.",
         location: "Kensington, Maryland, United States",
-        dedicated: "1974, November, 19",
+        dedication: "1974, November, 19",
         area: 156558,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
@@ -73,7 +73,7 @@ const temples = [
     {
         templeName: "Lima Perú",
         location: "Lima, Perú",
-        dedicated: "1986, January, 10",
+        dedication: "1986, January, 10",
         area: 9600,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
@@ -81,7 +81,7 @@ const temples = [
     {
         templeName: "Mexico City Mexico",
         location: "Mexico City, Mexico",
-        dedicated: "1983, December, 2",
+        dedication: "1983, December, 2",
         area: 116642,
         imageUrl:
             "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
@@ -89,7 +89,7 @@ const temples = [
     {
         templeName: "Austin Texas",
         location: "Texas, United States",
-        dedicated: "2024, August, 17",
+        dedication: "2024, August, 17",
         area: 30000,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/austin-texas-temple/austin-texas-temple-40361-main.jpg"
@@ -97,7 +97,7 @@ const temples = [
     {
         templeName: "Brigham City Utah",
         location: "Utah, United States",
-        dedicated: "2010, July, 31",
+        dedication: "2010, July, 31",
         area: 36000,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/brigham-city-utah-temple/brigham-city-utah-temple-39612-main.jpg"
@@ -105,7 +105,7 @@ const temples = [
     {
         templeName: "Chicago Illinois",
         location: "Illinois United States",
-        dedicated: "1983, August, 13",
+        dedication: "1983, August, 13",
         area: 37062,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/chicago-illinois-temple/chicago-illinois-temple-58403-main.jpg"
@@ -113,7 +113,7 @@ const temples = [
     {
         templeName: "Santiago Chile",
         location: "Santiago, Region Metropolitana, Chile",
-        dedicated: "2006, March, 12",
+        dedication: "2006, March, 12",
         area: 253015,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/_temp/024-Santiago-Chile-Temple.jpg"
@@ -121,7 +121,7 @@ const temples = [
     {
         templeName: "Casper Woyming",
         location: "Casper Wyoming, United States",
-        dedicated: "2024, November, 24",
+        dedication: "2024, November, 24",
         area: 9950,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/casper-wyoming-temple/casper-wyoming-temple-45007-main.jpg"
@@ -129,7 +129,7 @@ const temples = [
     {
         templeName: "Idaho Falls",
         location: "Idaho Falls, Idaho, United States",
-        dedicated: "1940, October, 19",
+        dedication: "1940, October, 19",
         area: 85624,
         imageUrl:
             "https://churchofjesuschristtemples.org/assets/img/temples/idaho-falls-idaho-temple/idaho-falls-idaho-temple-55801-main.jpg"
@@ -188,6 +188,7 @@ document.querySelector("#home").addEventListener("click", (e) => {
 
 document.querySelector("#old").addEventListener("click", (e) => {
     e.preventDefault();
+    pageTitle.textContent = "Old";
     // pageTitle.textContent = "Old (before 1900)";
     // createTempleCard(temples.filter(temple => { getDedicationYear(parseInt(temple.dedicated)) < 1900; console.log(temple) }));
     createTempleCard(temples.filter(temple => getDedicationYear(temple) < 1900));
@@ -195,19 +196,22 @@ document.querySelector("#old").addEventListener("click", (e) => {
 
 document.querySelector("#new").addEventListener("click", (e) => {
     e.preventDefault();
+    pageTitle.textContent = "New";
     // pageTitle.textContent = "New (after 2000)";
     createTempleCard(temples.filter(temple => getDedicationYear(temple) > 2000));
 });
 
 document.querySelector("#large").addEventListener("click", (e) => {
     e.preventDefault();
-    pageTitle.textContent = "Large (over 90000 sq ft)";
+    pageTitle.textContent = "Large";
+    // pageTitle.textContent = "Large (over 90000 sq ft)";
     createTempleCard(temples.filter(temple => temple.area > 90000));
 });
 
 document.querySelector("#small").addEventListener("click", (e) => {
     e.preventDefault();
-    pageTitle.textContent = "Small (under 10 000 sq ft)";
+    pageTitle.textContent = "Small";
+    // pageTitle.textContent = "Small (under 10 000 sq ft)";
     createTempleCard(temples.filter(temple => temple.area < 10000));
 });
 
